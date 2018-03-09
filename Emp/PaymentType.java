30cd5b96417473cd250d3edf5e18549ee8d059e8
AnuR1234 <35266536+AnuR1234@users.noreply.github.com>
2018-03-10 02:30:39 +0530

    Add files via upload

---
+++ Emp/PaymentType.java
@@ -0,0 +1,87 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+//import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class PaymentType {
+
+	public static class CustsMapper extends
+			Mapper<LongWritable, Text, Text, Text> {
+		public void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+			String record = value.toString();
+			String[] parts = record.split(",");
+			context.write(new Text(parts[0]), new Text("custs\t" + parts[1]+"\t"+parts[2]));
+		}
+	}
+
+	public static class TxnsMapper extends
+			Mapper<LongWritable, Text, Text, Text> {
+		public void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+			String record = value.toString();
+			String[] parts = record.split(",");
+			context.write(new Text(parts[2]), new Text("txns\t" + parts[3]+"\t"+parts[8]));
+		}
+	}
+
+	public static class ReduceJoinReducer extends
+			Reducer<Text, Text, Text, Text> {
+		public void reduce(Text key, Iterable<Text> values, Context context)
+				throws IOException, InterruptedException {
+			String name="";
+			double totalcredit= 0.0;
+			double totalcash=0.0;
+			for (Text t : values) {
+				String parts[] = t.toString().split("\t");
+				if (parts[0].equals("txns")) {
+					
+					if(parts[2].equals("cash")){
+						
+						totalcash += Float.parseFloat(parts[1]);
+					}
+					else if(parts[2].equals("credit")){
+						
+						totalcredit += Float.parseFloat(parts[1]);
+					}
+				} else if (parts[0].equals("custs")) {
+					
+					String fname = parts[1];
+					String lname=parts[2];
+					name=fname+','+lname;
+				}
+			}
+			String str = String.format("Cash is :%f \t Credit amt is:%f",totalcash ,totalcredit);
+			context.write(new Text(name), new Text(str));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf);
+	    job.setJarByClass(ReduceJoin.class);
+	    job.setJobName("Reduce Side Join");
+	    job.setReducerClass(ReduceJoinReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		//job.setNumReduceTasks(0);
+		MultipleInputs.addInputPath(job, new Path(args[0]),TextInputFormat.class, CustsMapper.class);
+		MultipleInputs.addInputPath(job, new Path(args[1]),TextInputFormat.class, TxnsMapper.class);
+		
+		Path outputPath = new Path(args[2]);
+		FileOutputFormat.setOutputPath(job, outputPath);
+		//outputPath.getFileSystem(conf).delete(outputPath);
+		
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
